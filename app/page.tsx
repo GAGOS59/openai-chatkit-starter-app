2774b17ac4ecb49b888d3944f310ddda2d24a38e
GAGOS59 <genevieve.gagos@ecole-eft-france.fr>
2025-10-13 15:03:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+<nav className="flex gap-4 justify-center mt-4 mb-6">
+  <a
+    href="/"
+    className="px-4 py-2 rounded font-semibold text-[#0f3d69] hover:bg-blue-50 border border-transparent hover:border-[#0f3d69] transition"
+  >
+    Guide interactif EFT
+  </a>
+  <a
+    href="/chat-libre"
+    className="px-4 py-2 rounded font-semibold text-[#0f3d69] hover:bg-blue-50 border border-transparent hover:border-[#0f3d69] transition"
+  >
+    Chat libre EFT officielle
+  </a>
+</nav>
+
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import React, { useRef, useState, useEffect, FormEvent } from "react";
