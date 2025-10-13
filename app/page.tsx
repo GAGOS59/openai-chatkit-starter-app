9ee46fdc4f773dbcc54162b5448507d5e33a052f
GAGOS59 <genevieve.gagos@ecole-eft-france.fr>
2025-10-13 12:56:53 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -164,7 +164,7 @@ export default function Page() {
             who: "bot",
             text:
               "Étape 8 — Bravo pour le travail fourni. Félicitations pour cette belle avancée. " +
-              "Maintenant, accorde-toi un moment pour t'hydrater et te reposer un instant. Offre-toi ce moment ! " +
+              "Maintenant, accorde-toi un moment pour t'hydrater et te reposer un instant. Offre-toi ce moment ! " + <fr/>
               "Rappelle-toi que ce guide est éducatif et ne remplace pas un avis médical."
           }]);
           setStage("Clôture");
