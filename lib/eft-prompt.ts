f4ed731fc8c807074082ab80cb0059291e14b093
GAGOS59 <genevieve.gagos@ecole-eft-france.fr>
2025-10-25 22:00:33 +0200

    Add EFT_SYSTEM_PROMPT constant in eft-prompt.ts

---
+++ lib/eft-prompt.ts
@@ -0,0 +1,6 @@
+// application/lib/eft-prompt.ts
+import "server-only"; // ⛔ interdit tout import côté client
+
+export const EFT_SYSTEM_PROMPT = `
+… colle ici ton prompt EFT complet (< 8000 caractères) …
+`;
