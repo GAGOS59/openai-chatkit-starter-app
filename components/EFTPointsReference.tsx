de5c2d6356683cfa8ad28583dd40912a2a6f6691
GAGOS59 <genevieve.gagos@ecole-eft-france.fr>
2025-11-03 11:53:42 +0100

    Fix HTML entity for apostrophe in text

--- components/EFTPointsReference.tsx
+++ components/EFTPointsReference.tsx
@@ -18,7 +18,7 @@ export default function EFTPointsReference({ className = "" }: { className?: str
         <div>
           <div style={{ fontWeight: 600 }}>Repères visuels : points EFT (photo + localisation)</div>
           <div style={{ fontSize: 13, color: "#555" }}>
-            Voir la photo des points et la description précise (s'ouvre dans un nouvel onglet).
+            Voir la photo des points et la description précise (s&apos;ouvre dans un nouvel onglet).
           </div>
         </div>
       </a>
